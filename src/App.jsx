77302400240ae223017a8fbe89f7d97b8b8793af
Gustavo H. Michels <ghmichels@gmail.com>
2022-07-11 18:50:20 -0300

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import { Post } from './Post'
+
+import styles from './App.module.css'
+import './global.css'
 
 export function App() {
 
   return (
     <>
-      <Post 
-        author="Gustavo Michels" 
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum blanditiis cupiditate quibusdam dolorem officiis aperiam corrupti porro aut, excepturi exercitationem eum iure doloribus possimus dolorum assumenda harum rem quisquam nostrum!"
-      />
-      <Post 
-        author="Fernando Monteiro" 
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum blanditiis cupiditate quibusdam dolorem officiis aperiam corrupti porro aut, excepturi exercitationem eum iure doloribus possimus dolorum assumenda harum rem quisquam nostrum!"
-      />
+      <Header />
+      
+      <div className={styles.wrapper}>
+        
+        <Sidebar />
+        <main>
+          <Post 
+            author="Gustavo Michels" 
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum blanditiis cupiditate quibusdam dolorem officiis aperiam corrupti porro aut, excepturi exercitationem eum iure doloribus possimus dolorum assumenda harum rem quisquam nostrum!"
+          />
+          <Post 
+            author="Fernando Monteiro" 
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum blanditiis cupiditate quibusdam dolorem officiis aperiam corrupti porro aut, excepturi exercitationem eum iure doloribus possimus dolorum assumenda harum rem quisquam nostrum!"
+          />
+        </main>
+      </div>
     </>
   )
 }
